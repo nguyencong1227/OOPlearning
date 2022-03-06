52f62b8aa03ff4e1baad133636e6df7a7c456e78
Công Nguyễn <76642260+nguyencong1227@users.noreply.github.com>
2022-03-06 08:10:26 -0500

    ex week2
    fibonacci

---
+++ fibonacci.java
@@ -0,0 +1,12 @@
+public class fibonacci {
+	public static void main (String [] args) {
+		int n = 99;
+		System.out.println(fib(n));
+	}
+
+	static long fib(int n) {
+		if (n <= 1)
+			return n;
+		return fib(n-1) + fib(n-2);
+	}
+}
