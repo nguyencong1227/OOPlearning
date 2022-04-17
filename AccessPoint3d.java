4dbe51efc59e49526bd4dcaa6570999cee491679
nguyencong1227 <nguyenchiconganh12@gmail.com>
2022-04-17 10:12:20 +0700

    update

--- AccessPoint3d.java
+++ AccessPoint3d.java
@@ -2,11 +2,11 @@ public class AccessPoint3d {
     public static void main(String[] args) {
         Point3d p1 = new Point3d(1, 1, 1);
         Point3d p2 = new Point3d(1, 6, 4);
-        Point3d p3 = new Point3d(0, 9, 6);
+        Point3d p3 = new Point3d(2, 9, 6);
         Point3d p4 = new Point3d(1,2,3);
 
-        equation_plane(p1, p2, p3);
-        CheckPoint3d(p1, p2, p3, p4);
+        Point3d.equation_plane(p1, p2, p3);
+        Point3d.CheckPoint3d(p1, p2, p3, p4);
 
 
     }
--- Point3d.java
+++ Point3d.java
@@ -6,7 +6,7 @@ public class Point3d extends Point {
     double z;
 
     public Point3d(double x, double y, double z) {
-        super(x, y);
+        super();
         this.z = z;
     }
 
