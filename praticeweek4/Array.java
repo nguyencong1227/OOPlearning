150c272d004d1ef95433fffafe0a9b2963fa24b9
nguyencong1227 <nguyenchiconganh12@gmail.com>
2022-05-08 09:44:56 +0700

    ex 1

---
+++ praticeweek4/Array.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Array {
+    public static ArrayList<Integer> readFile() throws FileNotFoundException {
+        // BufferedReader abc = new BufferedReader(new FileReader("./Bai1/dayso.txt"));
+        // List<String> lines = new ArrayList<String>();
+
+        // String line;
+        // while((line = abc.readLine()) != null) {
+        // lines.add(line);
+        // }
+        // abc.close();
+
+        // String[] data = lines.toArray(new String[]{});
+        // System.out.println(data[0]);
+        // return data;
+        File txt = new File("E:/SAVE/LapTrinhHuongDoiTuong/Code/week4/OOPlearning/praticeweek4/dayso.txt");
+        Scanner scan = new Scanner(txt);
+        ArrayList<Integer> data = new ArrayList<Integer>();
+        while (scan.hasNextInt()) {
+            data.add(scan.nextInt());
+        }
+        return data;
+    }
+
+    public static void main(String[] args) throws IOException {
+        ArrayList<Integer> data = readFile();
+        data.sort(null);
+        try {
+            FileWriter  myWriter  = new FileWriter("E:/SAVE/LapTrinhHuongDoiTuong/Code/week4/OOPlearning/praticeweek4/sapxep.txt");
+            for (int i = 0; i < data.size(); i++) {
+                myWriter.write(data.get(i) + " ");
+            }
+            myWriter.close();
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+}
